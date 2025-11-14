4535f565e425f896131e7f9406d3de3f87d19f67
itsnicolearias <nicolmccann@gmail.com>
2025-11-14 12:17:35 -0300

    fix: vercel errors

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -1,11 +1,13 @@
 /* eslint-disable no-console */
 import { Sequelize } from "sequelize"
 import { config } from "./config"
+import pg from "pg"
 
 const url = config.env === "test" ? config.dbTestUrl : config.dbUrl
 
 const sequelize = new Sequelize(url || "", {
   dialect: "postgres",
+  dialectModule: pg,
   logging: config.env === "development" ? console.log : false,
   pool: {
     max: 5,
